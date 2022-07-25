46d9f9879c50d3668e685e4f9e4d9e7df9c41ba9
chanseop <99310356+chanseop@users.noreply.github.com>
2022-07-25 18:12:48 +0900

    Create 2577.java

---
+++ 2577.java
@@ -0,0 +1,18 @@
+package baekjoon;
+import java.util.Scanner;
+public class no_2577 {
+	public static void main(String args[]) {
+		Scanner sc=new Scanner(System.in);
+		int a=sc.nextInt();
+		int b=sc.nextInt();
+		int c=sc.nextInt();
+		int sum=0;
+		sum=a*b*c;
+		
+		
+		
+		
+		
+		sc.close();
+	}
+}
