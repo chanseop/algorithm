4cbaca2ad66959dac73625ebdf6849c80ccd7915
chanseop <99310356+chanseop@users.noreply.github.com>
2023-10-16 08:30:15 +0900

    [level unrated] Title: 중앙값 구하기, Time: 0.36 ms, Memory: 75 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120811. 중앙값 구하기/중앙값 구하기.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int[] array) {
+        int answer = 0;
+        int center=0;
+        Arrays.sort(array);
+        center=array.length/2;
+        answer=array[center];
+        // System.out.println("center index:"+center + " answer value:" + answer);
+        return answer;
+    }
+}
