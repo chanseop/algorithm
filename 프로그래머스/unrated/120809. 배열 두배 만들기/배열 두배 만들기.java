365394cb7f677f38c06889f004a0d27c1c4a75aa
chanseop <99310356+chanseop@users.noreply.github.com>
2023-10-15 14:44:43 +0900

    [level unrated] Title: 배열 두배 만들기, Time: 2.06 ms, Memory: 78.1 MB -BaekjoonHub

--- 프로그래머스/unrated/120809. 배열 두배 만들기/배열 두배 만들기.java
+++ 프로그래머스/unrated/120809. 배열 두배 만들기/배열 두배 만들기.java
@@ -1,9 +1,20 @@
+// class Solution {
+//     public int[] solution(int[] numbers) {
+//         int[] answer = {};
+//         for(int i=0; i<numbers.length; i++){
+//             numbers[i]=2*numbers[i];
+//         }
+//         return numbers;
+//     }
+// }
+
+import java.util.Arrays;
+
 class Solution {
-    public int[] solution(int[] numbers) {
-        int[] answer = {};
-        for(int i=0; i<numbers.length; i++){
-            numbers[i]=2*numbers[i];
-        }
-        return numbers;
+    public int[] solution(int[] numbers){
+        return Arrays.stream(numbers).map(i -> i*2).toArray();
     }
 }
+
+
+
