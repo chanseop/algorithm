8d7cef5f3443d3965ec953fde13a98613f0b40a5
chanseop <99310356+chanseop@users.noreply.github.com>
2024-06-03 16:38:17 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 40 ms, Memory: 31252 KB -BaekjoonHub

---
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.py
@@ -0,0 +1,14 @@
+import sys
+
+input = sys.stdin.readline
+
+n = int(input())
+
+memo = [1]*n
+
+if n>=2:
+    memo[1]=3
+    for i in range(2,n):
+        memo[i] = (memo[i-1]+(2*memo[i-2]))
+
+print(memo[-1]%10007)
